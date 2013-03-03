61e410a6e69c04f6d2ac82eed8e111b47c06cce7
Cianan Sims <ctasims@gmail.com>
2013-03-02 21:30:38 -0700

    rollback that WTF commit

--- Ant.h
+++ Ant.h
@@ -26,21 +26,13 @@ class Ant
             search_direction = 360.0f;
         }
 
-  int x, y;
-  int rfidx, rfidy; // ant's rfid reader's x and y coordinates -- rfid reader trails behind antbot as it searches
-  int return_x, return_y; // coordinates of last seed found
-  int prevx, prevy; // location ant moved from on previous search move.
-  int search_time; // time since ant began its search
-  int search_time_counter,travel_time_counter,return_time_counter;
-  int ant_status; // 0 = no ant; 1 = ant following trail; 2 = ant carrying food; 3 = searching ant; 4 = traveling ant
-  bool influenceable; // ant will be influenced by any pheromone trails at nest entrance? true = ants will follow pheromones if present instead of walk at random or return to patch
-  int carrying; // food type ant is carrying: 0 = no food; # = foot item of distribution #
-  int since_move; // time steps since a searching ant last moved.
-  bool pen_down; // false = ant not laying a trail; true = ant laying a trail
-  float prev_pher; // Amount of pheromone on previous cell as ant moved along trail.
-  float prev_pher_scent; // The out-going sum_pheromone an detected on the previous move by an ant following a trail.
-  float direction; // direction an ant who detected no trail at nest is traveling.  value in degrees.
-  float search_direction; // direction in degrees that a searching ant moved in the previous time step (ideal direction, not grid direction)
+        int x, y;
+        int rfidx, rfidy;       // ant's rfid reader's x and y coordinates -- rfid reader trails behind antbot as it searches
+        int return_x, return_y; // coordinates of last seed found
+        int prevx, prevy;       // location ant moved from on previous search move.
+        int search_time;        // time since ant began its search
+        int search_time_counter,travel_time_counter,return_time_counter;
+        int ant_status;         // 0 = no ant; 1 = ant following trail; 2 = ant carrying food; 3 = searching ant; 4 = traveling ant
 
         // ant will be influenced by any pheromone trails at nest entrance?
         // true = ants will follow pheromones if present instead of walk at random or return to patch
